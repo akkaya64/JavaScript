15089c7b83035ef1ad533439128de47ce3de1cd9
fatih <akkaya064@gmail.com>
2023-08-14 19:22:03 +0100

    DOCUMENT OBJECT MODEL — 1

---
+++ day05/js/19-dom.js
@@ -0,0 +1,11 @@
+console.log(document);
+const h1El = document.querySelector('h1');
+console.log(h1El);
+const aboutUsEl = document.querySelector(`#aboutUs`);
+console.log(aboutUsEl);
+const spanEl = document.querySelector(`#aboutUs span`);
+console.log(spanEl);
+const menuEl = document.querySelector(`ul.menu`);
+console.log(menuEl);
+const menuItem3 = document.querySelector(`ul.menu li:nth-child(3)`);
+console.log(menuItem3);
