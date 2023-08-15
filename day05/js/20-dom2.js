6b618d3599c1116918c7510e166dc66be4b7b909
fatih <akkaya064@gmail.com>
2023-08-15 14:47:39 +0100

    DOCUMENT OBJECT MODEL - 2

---
+++ day05/js/20-dom2.js
@@ -0,0 +1,14 @@
+const h1E1 = document.querySelector(`#pageTitle`);
+console.log(h1E1)
+
+console.log(h1E1.classList)
+
+h1E1.classList.add("title")
+
+/* const aboutE1 = document.querySelector('#about').classList.add("bg-primary") */
+
+const aboutE1 = document.querySelector('#about');
+
+aboutE1.classList.add('bg-primary');
+aboutE1.classList.remove('bg-primary');
+
