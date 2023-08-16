9a996f030b3e395d2382853be8de93a491348ccd
fatih <akkaya064@gmail.com>
2023-08-16 20:33:12 +0100

    TERNARY

---
+++ day07/js/30-ternary.js
@@ -0,0 +1,21 @@
+let salary = 30000;
+
+console.log(salary > 10000 ? "You'll live":"You'll not good");
+
+console.log(salary > 30000
+    ? "Good"
+    : salary > 20000
+    ? "so so"
+    : salary > 10000
+    ? "Not good"
+    :"It's worse");
+
+    salary > 30000
+    ? console.log("good")
+    :console.log("Bed");
+
+    const salaryDeclaration = salary > 30000 ? "good" :"Not good";
+    console.log(salaryDeclaration);
+
+    const bodyE1 = document.querySelector("body");
+    bodyE1.innerText = salaryDeclaration;
