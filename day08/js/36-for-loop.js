c0dcc49940b4a2d6ea313dd2cee5f58baeba3e83
fatih <akkaya064@gmail.com>
2023-08-19 15:16:21 +0100

    FOR LOOP

--- day08/js/36-for-loop.js
+++ day08/js/36-for-loop.js
@@ -5,3 +5,47 @@ console.log("AYRAC")
 for (let i = 0; i < 10; i++) {
     console.log(i + 1);
 }
+
+console.log("AYRAC")
+
+let j = 0;
+for (; j < 10; j++) {
+    console.log("Hello World")
+}
+
+console.log("AYRAC")
+
+for (let i = 0; i < 10; i++) {
+    console.log(`${i}. kisinin adini giriniz`)
+}
+
+console.log("AYRAC ALTERNATIF 2")
+
+for (let i = 1; i < 11; i++) {
+    console.log(`${i}. kisinin adini giriniz`)
+}
+
+console.log("AYRAC ALTERNATIF 3")
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i}. kisinin adini giriniz`)
+}
+
+console.log("AYRAC ALTERNATIF 4")
+
+for (let i = 0; i < 10; i++) {
+    console.log(`${i+1}. kisinin adini giriniz`)
+}
+
+console.log("AYRAC ALTERNATIF 5")
+
+for (let i = 0; i < 10;) {
+    console.log(`${i++}. kisinin adini giriniz`)
+}
+
+console.log("AYRAC ALTERNATIF 6")
+
+for (let i = 0; i < 10;) {
+    console.log(`${++i}. kisinin adini giriniz`)
+}
+
